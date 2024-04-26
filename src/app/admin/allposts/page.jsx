aa5ce2c3096564e5c6e2a6c052ac55b84e2a39ca
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2024-04-26 13:12:29 +0545

    Basic things changed

--- src/app/admin/allposts/page.jsx
+++ src/app/admin/allposts/page.jsx
@@ -1,10 +1,45 @@
 'use client'
 
-const AllPosts = ()=>
+import { useState, useEffect } from "react";
+
+
+
+const AllPosts = () => 
 {
-    return(
+    const [blogpostsadmin, setBlogPostsAdmin] = useState([]);
+
+    const getAllPostsAdmin = async () => {
+        const req = await fetch('http://localhost:3000/api/blogs');
+        const res = await req.json();
+        setBlogPostsAdmin(res.result);
+    };
+
+    useEffect(() => {
+        getAllPostsAdmin()
+    }, []);
+    console.log(blogpostsadmin)
+
+
+    return (
         <>
-        <h1>All Posts</h1>
+            <div>
+                <div>
+                    {blogpostsadmin.length > 1 ?(
+                        <>
+                        {blogpostsadmin.map((blog)=>
+                    {
+                        <>
+
+                        </>
+                    })}
+                        </>
+                    ):(
+                        <>
+                        <h1>Loading...</h1>
+                        </>
+                    )}
+                </div>
+            </div>
         </>
     )
 }
